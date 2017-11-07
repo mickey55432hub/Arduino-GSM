1ed7c57c6a11f750b75a06f4294315e1e5b8bb3d
Shivam kumar tiwari <shivam55432@rediffmail.com>
2017-11-08 00:31:22 +0530

    Update Arduino-GSM.ino

--- Arduino-GSM.ino
+++ Arduino-GSM.ino
@@ -1,30 +1,70 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial mySerial(9, 10);
+#define TRIGGER_PIN  7  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     8  // Arduino pin tied to echo pin on the ultrasonic sensor.
+
 
 void setup()
 {
   mySerial.begin(9600);   // Setting the baud rate of GSM Module  
+  pinMode(6, OUTPUT);      // initialize digital pin 13 as an output.
   Serial.begin(9600);    // Setting the baud rate of Serial Monitor (Arduino)
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
   delay(100);
+  
 }
+
+
+
+
+
+
+
+
 
 
 void loop()
 {
-  if (Serial.available()>0)
-   switch(Serial.read())
-  {
-    case 's':
-      SendMessage();
-      break;
-    case 'r':
-      RecieveMessage();
-      break;
-  }
+  float duration, distance;
+  digitalWrite(TRIGGER_PIN, LOW);
+  delayMicroseconds(2); 
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(TRIGGER_PIN, LOW);
+  duration = pulseIn(ECHO_PIN, HIGH);
+  distance = (duration/2) / 29.1;
+    Serial.print(" Ping: ");
+    Serial.print(distance); // Convert ping time to distance in cm and print result (0 = outside set distance range)
+    Serial.println("cm");
 
- if (mySerial.available()>0)
+    if ((distance>0)&&(distance<20))
+     {
+      {
+  digitalWrite(6, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);              // wait for a second
+  digitalWrite(6, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);  
+  SendMessage();
+}
+     }
+
+
+ if ((distance>30)&&(distance<240))
+     {
+      {
+  digitalWrite(6, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(800);              // wait for a second
+  digitalWrite(6, LOW);    // turn the LED off by making the voltage LOW
+  delay(800);  
+  SendMessage();
+}
+     }
+
+if (mySerial.available()>0)
    Serial.write(mySerial.read());
+
 }
 
 
@@ -46,3 +86,7 @@ void loop()
   mySerial.println("AT+CNMI=2,2,0,0,0"); // AT Command to receive a live SMS
   delay(1000);
  }
+
+
+
+
